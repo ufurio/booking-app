31d9b2812eff9449e100830774c45474d3a2d614
uryel <ffurio.uriel@gmail.com>
2021-08-01 01:20:05 +0800

    added a build folder

--- components/Form/Form.js
+++ components/Form/Form.js
@@ -43,106 +43,113 @@ function Form() {
   }, [formData.date]);
 
   return (
-    <form className={styles.form}>
-      {response ? <div className={styles.response}>{response}</div> : null}
+    <>
       <Link href="/appointments" passHref>
         <button>appointments</button>
       </Link>
-      <div className={styles.formGroup}>
-        <label htmlFor="firstName" className={styles.formLabel}>
-          First Name
-        </label>
-        <input
-          type="text"
-          name="firstName"
-          className={styles.formInput}
-          placeholder="Enter your first name"
-          value={formData.firstName}
-          onChange={handleChange}
-          required
-        />
-      </div>
-      <div className={styles.formGroup}>
-        <label htmlFor="lastName" className={styles.formLabel}>
-          Last Name
-        </label>
-        <input
-          type="text"
-          name="lastName"
-          className={styles.formInput}
-          placeholder="Enter your last name"
-          value={formData.lastName}
-          onChange={handleChange}
-          required
-        />
-      </div>
+      <form className={styles.form}>
+        {response ? <div className={styles.response}>{response}</div> : null}
 
-      <div className={styles.formGroup}>
-        <label htmlFor="contactNumber" className={styles.formLabel}>
-          Contact No.
-        </label>
-        <div>
+        <div className={styles.formGroup}>
+          <label htmlFor="firstName" className={styles.formLabel}>
+            First Name
+          </label>
           <input
-            type="tel"
-            name="contactNumber"
-            style={{ width: "20%" }}
+            type="text"
+            name="firstName"
             className={styles.formInput}
-            placeholder="+63"
-            disabled
-          />
-          <input
-            type="tel"
-            name="contactNumber"
-            className={styles.formInput}
-            placeholder="Enter your contact number"
-            value={formData.contactNumber}
+            placeholder="Enter your first name"
+            value={formData.firstName}
             onChange={handleChange}
             required
           />
         </div>
-      </div>
-      <div className={styles.formGroup}>
-        <label htmlFor="date" className={styles.formLabel}>
-          Preferred Date
-        </label>
-        <input
-          type="date"
-          name="date"
-          value={formData.date}
-          className={styles.formInput}
-          placeholder="Pick your preferred date"
-          onChange={handleChange}
-          required
-        />
-      </div>
-      <div className={styles.formGroup}>
-        <label htmlFor="time" className={styles.formLabel}>
-          Preferred Time
-        </label>
-        <p className={styles.formLabelp}>
-          *choose a preferred date before choosing your time to update the time
-          slots options
-        </p>
-        <select name="time" onChange={handleChange} value={formData.time}>
-          <option>Choose your preferred time </option>
-          {timeSlots ? (
-            timeSlots.map((timeSlot) => {
-              return (
-                <option key={timeSlot.time} value={timeSlot.time}>
-                  {timeSlot.time} has {timeSlot.count} slot/s left.
-                </option>
-              );
-            })
-          ) : (
-            <option>Choose your preferred time</option>
-          )}
-        </select>
-      </div>
+        <div className={styles.formGroup}>
+          <label htmlFor="lastName" className={styles.formLabel}>
+            Last Name
+          </label>
+          <input
+            type="text"
+            name="lastName"
+            className={styles.formInput}
+            placeholder="Enter your last name"
+            value={formData.lastName}
+            onChange={handleChange}
+            required
+          />
+        </div>
 
-      <button type="submit" className={styles.formButton} onClick={formSubmit}>
-        Submit
-      </button>
-    </form>
+        <div className={styles.formGroup}>
+          <label htmlFor="contactNumber" className={styles.formLabel}>
+            Contact No.
+          </label>
+          <div>
+            <input
+              type="tel"
+              name="contactNumber"
+              style={{ width: "20%" }}
+              className={styles.formInput}
+              placeholder="+63"
+              disabled
+            />
+            <input
+              type="tel"
+              name="contactNumber"
+              className={styles.formInput}
+              placeholder="Enter your contact number"
+              value={formData.contactNumber}
+              onChange={handleChange}
+              required
+            />
+          </div>
+        </div>
+        <div className={styles.formGroup}>
+          <label htmlFor="date" className={styles.formLabel}>
+            Preferred Date
+          </label>
+          <input
+            type="date"
+            name="date"
+            value={formData.date}
+            className={styles.formInput}
+            placeholder="Pick your preferred date"
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className={styles.formGroup}>
+          <label htmlFor="time" className={styles.formLabel}>
+            Preferred Time
+          </label>
+          <p className={styles.formLabelp}>
+            *choose a preferred date before choosing your time to update the
+            time slots options
+          </p>
+          <select name="time" onChange={handleChange} value={formData.time}>
+            <option>Choose your preferred time </option>
+            {timeSlots ? (
+              timeSlots.map((timeSlot) => {
+                return (
+                  <option key={timeSlot.time} value={timeSlot.time}>
+                    {timeSlot.time} has {timeSlot.count} slot/s left.
+                  </option>
+                );
+              })
+            ) : (
+              <option>Choose your preferred time</option>
+            )}
+          </select>
+        </div>
+
+        <button
+          type="submit"
+          className={styles.formButton}
+          onClick={formSubmit}
+        >
+          Submit
+        </button>
+      </form>
+    </>
   );
 }
 
